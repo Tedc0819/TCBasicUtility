26307f98e0f4c13d54b1627c5626f60da6e0084b
Ted Cheng <tedc890819@gmail.com>
2013-10-02 23:55:41 +0800

    first commit

---
+++ TCBasicUtility/TCBasicUtility.h
@@ -0,0 +1,13 @@
+//
+//  TCBasicUtility.h
+//  TCBasicUtility
+//
+//  Created by Ted Cheng on 2/10/13.
+//  Copyright (c) 2013 Ted Cheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TCBasicUtility : NSObject
+
+@end
---
+++ TCBasicUtility/TCBasicUtility.m
@@ -0,0 +1,13 @@
+//
+//  TCBasicUtility.m
+//  TCBasicUtility
+//
+//  Created by Ted Cheng on 2/10/13.
+//  Copyright (c) 2013 Ted Cheng. All rights reserved.
+//
+
+#import "TCBasicUtility.h"
+
+@implementation TCBasicUtility
+
+@end
